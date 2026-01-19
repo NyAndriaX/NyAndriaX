d3b5b3642564b5fbd9a4acc584591ace9d1bc207
NyAndriaX <tsilavinaandriamahafaly01@gmail.com>
2026-01-19 08:29:30 +0100

    refactor: Remove companies data export from constants and data index files to streamline project structure and improve clarity

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -5,7 +5,6 @@
 
 export {
   navigationData as NAVIGATION_ITEMS,
-  companiesData as COMPANY_LOGOS,
   technologiesData as TECH_LOGOS,
   heroData as HERO_CONTENT,
   logoText as LOGO_TEXT,
--- app/lib/data/index.ts
+++ app/lib/data/index.ts
@@ -5,7 +5,6 @@
 
 export { heroData } from "./hero";
 export { navigationData } from "./navigation";
-export { companiesData } from "./companies";
 export { technologiesData } from "./technologies";
 export { experienceData } from "./experience";
 export { projectsData, projectFilters, workDescription } from "./work";
